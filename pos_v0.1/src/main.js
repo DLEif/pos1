82bc977038a82cd0b02936392f32feb29cd51022
DLEif <181713574@qq.com>
2014-12-04 18:18:10 +0800

    project complete

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,65 +1,68 @@
-function printInventory(inputs) {
-  var tagArray = [];
-  var sutotal = 0;
-  var totalAmount = 0;
-  var inventoryText = '***<没钱赚商店>购物清单***\n';
+function printInventory(items) {
 
-  for(var i = 0; i < inputs.length; i++){
-    if(tagArray !== inputs[i].name){
-      tagArray.push({
-        borcade : inputs[i].barcode,
-        name : inputs[i].name,
-        unit: inputs[i].nuit,
-        price: inputs[i].price,
-        count : 1
-      });
-    }else{
-      tagArray[i].count++;
+  // 1. 循环items
+  // 2. 我们想把每一次循环的结果放在一个新的集合(cartItems)中用于统计
+  // 3. 从cartItems中寻找是否存在已有的cartItem
+  // 4. 如果cartItem存在，那么更新其数量(count)
+  // 5. 如果不存在，则添加一个新的cartItem对象(包括两个属性：item, count)
+
+  var cartItems = [];
+
+  for(var i = 0; i < items.length; i++) {
+    var cartItem = findCartItem(cartItems, items[i].barcode);
+    if (cartItem) {
+      cartItem.count++;
+    } else {
+      cartItems.push({ item: items[i], count: 1 });
     }
   }
 
-  for(var i = 0; i < tagArray.length; i++){
+  // 1. 我们已经嗯有了cartItems
+  // 2. 我们需要循环cartItems
+  // 3. 计算出每一个cartItem的小计(subtotal)
+  // 4. 生成这个cartItem的清单文字
+  // 5. 将小计累加，生成总金额(totalAmount)
+  // 6. 拼接以上所有结果，生成最终文本
+  // 7. 在Console中输出最终结果
 
-    var subtotal = getSubtotal(item.count, item.price);
 
-    totalAmount += sutotal;
-    inventoryText += '名称：' + item.name +
-                     '，数量：' + item.count + item.unit +
-                     '，单价：' + item.price.toFixed(2) +
-                     '(元)，小计：' + subtotal.toFixed(2) +
-                     '(元)\n';
+  var totalAmount = 0;
+  var cartItemsText = '';
+
+  for(var x = 0; x < cartItems.length; x++) {
+    var cartItem = cartItems[x];
+    var item = cartItem.item;
+
+    var subtotal = getSubtotal(item.price, cartItem.count);
+
+    cartItemsText += '名称：' + item.name +
+                       '，数量：' + cartItem.count + item.unit +
+                       '，单价：' + item.price.toFixed(2) +
+                       '(元)，小计：' + subtotal.toFixed(2) +
+                       '(元)\n';
+
+    totalAmount += subtotal;
   }
 
-  inventoryText += '----------------------\n' +
-                   '总计：' + totalAmount.toFixed(2) +
-                   '(元)\n' + '**********************'；
+  var inventoryText = '***<没钱赚商店>购物清单***\n' +
+                      cartItemsText +
+                      '----------------------\n' +
+                      '总计：' + totalAmount.toFixed(2) + '(元)\n' +
+                      '**********************';
 
   console.log(inventoryText);
 }
-//var count = 0;
-// for(var j = 0; j < tagArray.length; j++){
-//       if(tagArray[j] === inputs[i].name){
-//         tagArray = tagArray[j];
-//       }
-//     var tagArray = inputs[i].name;
-//
-//   }
-//   console.log(tagArray)
-// }
 
+function findCartItem(cartItems, barcode){
+  var cartItem;
+  for(var i = 0; i < cartItems.length; i++){
+    if(cartItems[i].item.barcode == barcode){
+      cartItem = cartItems[i];
+    }
+  }
+  return cartItem;
+}
 
-
-    // if(tagArray[i] === inputs[i].name){
-    //     tagArray[i].count++;
-    // }
-  //   var  inputs[i] = ('count = 0');
-  //   console.log(inputs);
-  //  }
-  // // for(var i = 0; i < inputs.length; i++){
-  //   if(inputs[i].barcode === tagArray){
-  //     tagArray.push = inputs[i];
-  //   }
-//     console.log(tagArray);
-//   }
-//   //return ;
-// }
+function getSubtotal(price, count){
+  return price * count;
+}
