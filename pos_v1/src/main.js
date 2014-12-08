cf94e29e31b10a473e44f706548d527356e0004b
DLEif <181713574@qq.com>
2014-12-08 23:30:18 +0800

    v1 pass

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,32 +1,59 @@
-function printInventory(barcodes){
-  var items = [];
-  // var arr = barcode[i].split('-');
-  var allItems = loadAllItems();
-
-  // for(var i = 0; i < barcode.length; i++){
-  //   var item = findItem(allItems, arr[0]);
-  //   items.push(item);
-  // }
-
+function printInventory(tags) {
+  // var invero = '***<没钱赚商店>购物清单***\n' +
+  // '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+  // '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+  // '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+  // '----------------------\n' +
+  // '挥泪赠送商品：\n' +
+  // '名称：雪碧，数量：1瓶\n' +
+  // '名称：方便面，数量：1袋\n' +
+  // '----------------------\n' +
+  // '总计：51.00(元)\n' +
+  // '节省：7.50(元)\n' +
+  // '**********************';
+  var cartItems = getcartItems(tags);
+  var inventorytext = getText(cartItems);
+  console.log(inventorytext);
+}
+function getcartItems(tags) {
   var cartItems = [];
-  //  var Inventory = getInventory();
-
-  for(var i = 0; i < barcodes.length; i++) {
-    var arr = barcodes[i].split('-');
-    barcode = arr[0];
+  var allItems = loadAllItems();
+  for(var i = 0; i < tags.length; i++) {
+    var splitArray = tags[i].split('-');
+    barcode = splitArray[0];
     count = 1;
-    if(arr[1]){
-      count = parseFloat(arr[1]);
+    if(splitArray[1]){
+      count = parseFloat(splitArray[1]);
     }
-    var cartItem = findCartItem(cartItems, barcode);
-    if(cartItem){
+    var cartItem = findCartItem(barcode, cartItems);
+    if(cartItem) {
       cartItem.count += count;
-    }else {
-      var newItem = findItem(allItems,barcode);
-      cartItems.push({ item: newItem, count: count });
+    } else {
+      var newitem = findItem(allItems,barcode);
+      cartItems.push({ item: newitem , count: count});
     }
   }
-  console.log(cartItems);
+  return cartItems;
+}
+function findCartItem(barcode, cartItems) {
+  var cartItem;
+  for(var i = 0; i < cartItems.length; i++) {
+    if(cartItems[i].item.barcode === barcode){
+      cartItem = cartItems[i];
+    }
+  }
+  return cartItem;
+}
+function findItem(allItems,barcode) {
+  var newitem;
+  for(var i = 0;i < allItems.length;i++) {
+    if(allItems[i].barcode === barcode) {
+      newitem = allItems[i];
+    }
+  }
+  return newitem;
+}
+function getText(cartItems) {
   var inventorytext = '***<没钱赚商店>购物清单***\n';
   var promotionText = '';
   var summaryText = '';
@@ -36,7 +63,7 @@ function printInventory(barcodes){
   var saveMoney = 0;
   var itemsText= '';
   for(var i = 0; i < cartItems.length; i++) {
-    var promotionItem= findPromotionItem(cartItems[i].item.barcode,promotions[0]);
+    var promotionitem = findpromotionitem(cartItems[i].item.barcode,promotions[0]);
     if( promotionitem) {
       promotionText += '名称：' + cartItems[i].item.name +
       '，数量：' + Math.floor(cartItems[i].count / 3)
@@ -46,7 +73,7 @@ function printInventory(barcodes){
   }
   for(var i = 0; i < cartItems.length; i++) {
     var count = cartItems[i].count;
-    varpromotionItem=findPromotionItem(cartItems[i].item.barcode,promotions[0]);
+    var promotionitem = findpromotionitem(cartItems[i].item.barcode,promotions[0]);
     if( promotionitem) {
       count = cartItems[i].count - Math.floor(cartItems[i].count / 3);
     }
@@ -68,36 +95,11 @@ function printInventory(barcodes){
   '**********************';
   return inventorytext;
 }
-
-function findItem(allItems,barcode){
-  var item;
-  for(var i = 0; i < allItems.length; i++){
-    if(allItems[i].barcode === barcode){
-      item = allItems[i];
-    }
-  }
-  return item;
-}
-
-function findCartItem(cartItems, barcode){
-  var cartItem;
-  for(var i = 0; i < cartItems.length; i++){
-    if(cartItems[i].item.barcode === barcode){
-      cartItem = cartItems[i];
-    }
-  }
-  return cartItem;
-}
-
-function getSubtotal(price, count){
-  return price * count;
-}
-
-function findPromotionItem(barcode,promotions) {
+function findpromotionitem(barcode,promotions) {
   var promotionitem;
   for(var i = 0;i < promotions.barcodes.length;i++) {
     if(promotions.barcodes[i] === barcode) {
-     promotionItem= promotions.barcodes[i];
+      promotionitem = promotions.barcodes[i];
     }
   }
   return promotionitem;
