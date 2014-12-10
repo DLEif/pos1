8dbb616c5eb21193834b31762fea789aab2e3297
DLEif <181713574@qq.com>
2014-12-10 20:18:34 +0800

    pos_v2 split out promotionText

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -50,21 +50,15 @@ function getText(cartItems) {
   var subtotal = 0;
   var saveMoney = 0;
   var itemsText= '';
-  for(var i = 0; i < cartItems.length; i++) {
-    var promotionitem = findpromotionitem(cartItems[i].item.barcode,promotions[0]);
-    if( promotionitem) {
-      promotionText += '名称：' + cartItems[i].item.name +
-      '，数量：' + Math.floor(cartItems[i].count / 3)
-      + cartItems[i].item.unit + '\n';
-      saveMoney += cartItems[i].item.price * Math.floor(cartItems[i].count / 3);
-    }
-  }
+  var promotionText = getpromotionText(cartItems);
+
   for(var i = 0; i < cartItems.length; i++) {
     var count = cartItems[i].count;
     var promotionitem = findpromotionitem(cartItems[i].item.barcode,promotions[0]);
     if( promotionitem) {
       count = cartItems[i].count - Math.floor(cartItems[i].count / 3);
     }
+    saveMoney += cartItems[i].item.price * Math.floor(cartItems[i].count / 3);
     subtotal = (cartItems[i].item.price) * count;
     sumtotal += subtotal;
     itemsText += '名称：' + cartItems[i].item.name +
@@ -82,6 +76,20 @@ function getText(cartItems) {
   + '节省：' + saveMoney.toFixed(2) + '(元)\n'+
   '**********************';
   return inventorytext;
+}
+function getpromotionText(cartItems){
+  var promotionText = '';
+  var promotions = loadPromotions();
+
+  for(var i = 0; i < cartItems.length; i++) {
+    var promotionitem = findpromotionitem(cartItems[i].item.barcode,promotions[0]);
+    if( promotionitem) {
+      promotionText += '名称：' + cartItems[i].item.name +
+      '，数量：' + Math.floor(cartItems[i].count / 3)
+      + cartItems[i].item.unit + '\n';
+    }
+  }
+  return promotionText;
 }
 function findpromotionitem(barcode,promotions) {
   var promotionitem;
